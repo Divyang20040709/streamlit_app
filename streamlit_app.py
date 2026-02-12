fb3aec3465210698eeae9663add081e161423461
Divyang Solanki <divyangsolanki2004@gmail.com>
2026-02-12 14:09:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,19 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
 
-file = st.file_uploader("Upload a file")
+file = st.file_uploader("Upload CSV", type=["csv"])
 
 if file is not None:
     df = pd.read_csv(file)
-    st.write(df)
 
-    product_name = df.groupby('Product Name')['Quantity'].agg(['count', 'sum', 'mean', 'min', 'max']).reset_index()
-    st.write(product_name)
+    product_name = df.groupby('Product Name')['Quantity'] \
+                     .agg(['count', 'sum', 'mean', 'min', 'max']) \
+                     .reset_index()
 
-    top_10 = product_name.nlargest(10, 'sum')
-    st.write(top_10)
+    top_10 = product_name.sort_values('sum', ascending=False).head(10)
+
+    st.subheader("Top 10 Products by Quantity Sold")
+    st.bar_chart(top_10, x='Product Name', y='sum')
 
 else:
     st.write("Please upload a CSV file.")
-
-
-
-st.bar_chart(top_10, x='Product Name', y='sum')
-st.line_chart(top_10, x='Product Name', y='sum')
